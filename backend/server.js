f91caf7a76755a78fa2143fe4a41af65897eed2f
Ole Jacob Nyborg-Christensen <olenybchr@protonmail.com>
2020-01-11 21:52:14 +0200

    a

--- backend/server.js
+++ backend/server.js
@@ -83,11 +83,13 @@ const {
 
     // ... other app.use middleware
     if (process.env.NODE_ENV === "production") {
-      app.use(express.static(path.join(__dirname, "frontend", "build")));
+      app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
     }
 
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+      res.sendFile(
+        path.join(__dirname, "..", "frontend", "build", "index.html")
+      );
     });
 
     app.use(function(err, req, res, next) {
