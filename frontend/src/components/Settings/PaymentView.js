1e290ae314aac5b603154927d90f00fbaef8a472
Ole Jacob Nyborg-Christensen <olenybchr@protonmail.com>
2020-02-14 11:45:37 +0100

    Bruh

--- frontend/src/components/Settings/PaymentView.js
+++ frontend/src/components/Settings/PaymentView.js
@@ -9,14 +9,20 @@ const StripeView = () => {
     return (
       <div className="padding-10">
         <StripeConnect />
+        <p className="color-gray text-center">
+          We take a 10% fee + 59¢ for each plan sold.
+        </p>
       </div>
     );
   }
 
   return (
     <div>
-      Go to your Stripe Dashboard to view details. If you have any questions
-      email us at payment@chadify.me We take a 10% fee + 59¢ for each plan sold.
+      <ul>
+        <li>Go to your Stripe Dashboard to view details.</li>
+        <li>If you have any questions email us at payment@chadify.me</li>
+        <li>We take a 10% fee + 59¢ for each plan sold.</li>
+      </ul>
     </div>
   );
 };
